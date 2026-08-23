ccafa68e2c507a700e00d057a41f64cf6a58e817
unknown <alzarrar007@gmail.com>
2026-08-23 15:40:16 +0500

    Tinymce configuration

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -10,7 +10,7 @@ function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-          apiKey="no-api-key"
+          apiKey="dda8m32tpe2ktxe0nr5hngia95a8p3o91uwg76qmrmbokjps"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,

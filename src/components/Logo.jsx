b0c56ace99063826cd22f1640af49915dcf5aac3
unknown <alzarrar007@gmail.com>
2026-08-24 11:41:48 +0500

    logo error

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,10 @@
  
-import storiqologo from "../assets/storiqo-logo.png";
+import storiqoLogo from "../assets/storiqo-logo.png";
 
 
 function Logo({ className ='' }) {
   return <img 
-  src={storiqologo} 
+  src={storiqoLogo} 
   alt="Logo"
   
   className={className}

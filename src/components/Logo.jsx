ab5a24de748a3c35671300fd0918031c28f6ff92
unknown <alzarrar007@gmail.com>
2026-08-24 12:03:14 +0500

    added border to logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,9 +4,9 @@
 
 function Logo({ className ='' }) {
   return (
-    <div className={`flex flex-col gap-y-1 p-1 ${className}`}>
+    <div className={`flex flex-col p-1 border border-teal-500 rounded ${className}`}>
          <h1 className="text-2xl text-teal-500 font-semibold font-mono">storiqo</h1>
-         <span className="text-white font-light">where stories begin</span>
+         <span className="text-white font-light font-mono">where stories begin</span>
     </div>
   );
 }

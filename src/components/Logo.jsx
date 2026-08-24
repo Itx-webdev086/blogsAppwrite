d78384b643b03c5e73889d27c379165ae1857bab
unknown <alzarrar007@gmail.com>
2026-08-24 12:05:03 +0500

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,9 +4,9 @@
 
 function Logo({ className ='' }) {
   return (
-    <div className={`flex flex-col p-1 border border-teal-500 rounded ${className}`}>
+    <div className={`flex flex-col py-1 px-3 border border-teal-500 rounded ${className}`}>
          <h1 className="text-2xl text-teal-500 font-semibold font-mono">storiqo</h1>
-         <span className="text-white font-light font-mono">where stories begin</span>
+         <span className="text-white font-thin font-mono">where stories begin</span>
     </div>
   );
 }

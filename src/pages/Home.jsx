bb439dffaed6fa2d4b73c1c7c2f5924ebed25686
Itx-webdev <alzarrar007@gmail.com>
2026-09-02 12:04:31 +0500

    hero heaeding fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,9 +21,9 @@ function Home() {
     return (
         <div className='mt-18 h-screen w-full'>
         <div className='flex justify-start items-center p-10 h-[90%] w-full' style={{ backgroundImage: `url(${Hero})` }}>
-         <div className="space-y-5 w-xl">
-            <div className='text-8xl text-gray-800 font-extrabold font-serif'>
-                <h1>Your <br></br> thoughts & stories</h1>
+         <div className="space-y-5 w-2xl">
+            <div className='text-gray-800 font-serif'>
+                <h1 className='text-7xl font-extrabold'>Your <br></br> thoughts & stories</h1>
                 <p className="font-light text-gray-800">A place where stories begin, and people connect.</p>
             </div>
             <div className='flex justify-start items-center gap-4'>

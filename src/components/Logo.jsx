1fdd66c03d785919d54c8e00cbaa5f0b5a086c75
Itx-webdev <alzarrar007@gmail.com>
2026-09-02 11:53:57 +0500

    home page and logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@
 function Logo({ className ='' }) {
   return (
     <div className={`mr-10 ${className}`}>
-         <h1 className="text-2xl text-white font-extrabold font-serif">Connection</h1>
+         <h1 className="text-2xl text-white font-extrabold font-serif space-x-1">Connection</h1>
     </div>
   );
 }
